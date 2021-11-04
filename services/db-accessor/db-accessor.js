332312912d842625525cbb5baef631620fc8fb88
Ilya <ilyah@codevalue.net>
2021-11-04 18:11:26 +0200

    added db-accessor

---
+++ services/db-accessor/db-accessor.js
@@ -0,0 +1,138 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const path = require('path');
+const Joi = require('joi');
+
+const db = require("./db");
+const messagesCollectionName = "messages";
+const usersCollectionName = "users";
+const app = express();
+
+// parses json data sent to us by the user
+app.use(bodyParser.json());
+
+/*****************************************************************
+ * Messages Controller
+ *****************************************************************/
+
+// schema used for data validation for our todo document
+const messageSchema = Joi.object().keys({
+    userId: Joi.string().required(),
+    content: Joi.string().required()
+});
+
+// read
+app.get(`/${messagesCollectionName}`,(req,res)=>{
+    db.getDB().collection(messagesCollectionName).find({}).toArray((err,documents)=>{
+        if(err)
+            console.log(err);
+        else{
+            res.json(documents);
+        }
+    });
+});
+
+//create
+app.post(`/${messagesCollectionName}`,(req,res,next)=>{
+    // Document to be inserted
+    const userInput = req.body;
+    console.log('Inserting a new message to DB');
+    console.log(JSON.stringify(req.body));
+
+    Joi.validate(userInput,messageSchema,(err,result)=>{
+        if(err){
+            const error = new Error("Invalid Input");
+            error.status = 400;
+            next(error);
+        }
+        else{
+            db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
+                if(err){
+                    const error = new Error("Failed to add message");
+                    error.status = 400;
+                    next(error);
+                }
+                else
+                    res.json({result : result, document : result.ops[0],msg : "Successfully added message!!!",error : null});
+            });
+        }
+    })
+});
+
+
+/*****************************************************************
+ * Users Controller
+ *****************************************************************/
+
+// schema used for data validation for our todo document
+const userSchema = Joi.object().keys({
+    nickName: Joi.string().required(),
+    email: Joi.string().required()
+});
+
+
+// read
+app.get(`/${usersCollectionName}`,(req,res)=>{
+    db.getDB().collection(messagesCollectionName).find({}).toArray((err,documents)=>{
+        if(err)
+            console.log(err);
+        else{
+            res.json(documents);
+        }
+    });
+});
+
+//create
+app.post(`/${usersCollectionName}`,(req,res,next)=>{
+    // Document to be inserted
+    const userInput = req.body;
+    console.log('Inserting a new user to DB');
+    console.log(JSON.stringify(req.body));
+
+    Joi.validate(userInput,schema,(err,result)=>{
+        if(err){
+            const error = new Error("Invalid Input");
+            error.status = 400;
+            next(error);
+        }
+        else{
+            db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
+                if(err){
+                    const error = new Error("Failed to add user");
+                    error.status = 400;
+                    next(error);
+                }
+                else
+                    res.json({result : result, document : result.ops[0],msg : "Successfully added message!!!",error : null});
+            });
+        }
+    })
+});
+
+// Middleware for handling Error
+// Sends Error Response Back to User
+app.use((err,req,res,next)=>{
+    res.status(err.status).json({
+        error : {
+            message : err.message
+        }
+    });
+})
+
+
+db.connect((err)=>{
+    // If err unable to connect to database
+    // End application
+    if(err){
+        console.log('unable to connect to database');
+        process.exit(1);
+    }
+    // Successfully connected to database
+    // Start up our Express Application
+    // And listen for Request
+    else{
+        app.listen(8002,()=>{
+            console.log('connected to database, Registry listening on port 8002');
+        });
+    }
+});
---
+++ services/db-accessor/db.js
@@ -0,0 +1,42 @@
+const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const dbname = "registry-db";
+const url = "mongodb://mongo:27017";
+const mongoOptions = {useNewUrlParser : true};
+
+const state = {
+    db : null
+};
+
+const connect = (cb) =>{
+    // if state is not NULL
+    // Means we have connection already, call our CB
+    if(state.db)
+        cb();
+    else{
+        // attempt to get database connection
+        MongoClient.connect(url,mongoOptions,(err,client) => {
+            // unable to get database connection pass error to CB
+            if(err)
+                cb(err);
+            // Successfully got our database connection
+            // Set database connection and call CB
+            else{
+                state.db = client.db(dbname);
+                cb();
+            }
+        });
+    }
+}
+
+// returns OBJECTID object used to
+const getPrimaryKey = (_id) => {
+    return ObjectID(_id);
+}
+
+// returns database connection
+const getDB = () => {
+    return state.db;
+}
+
+module.exports = {getDB,connect,getPrimaryKey};
