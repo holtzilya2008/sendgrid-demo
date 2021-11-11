c2a6aab14d5f9c384532d8cbc15cf0fcc596d0e0
Ilya <ilyah@codevalue.net>
2021-11-12 01:23:22 +0200

    restored sending email code

--- services/backend/src/services/messages.service.ts
+++ services/backend/src/services/messages.service.ts
@@ -1,14 +1,16 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
+import { EmailData } from '@sendgrid/helpers/classes/email-address';
 import { AxiosResponse } from 'axios';
 import { firstValueFrom, map, tap } from 'rxjs';
 
-import { DB_ACCESSOR_URL, DOMAIN_NAME, SENDGRID_API_KEY } from 'src/constants';
+import { CHAT_NAME, DB_ACCESSOR_URL, DOMAIN_NAME, SENDGRID_API_KEY } from 'src/constants';
 import { CreateMessageDTO } from 'src/dto/create-message-dto';
 import { CreateMessageResponseDTO } from 'src/dto/create-message-response-dto';
 import { EmailDTO } from 'src/dto/email-dto';
 import { MessageDTO } from 'src/dto/message-dto';
+import { UserDTO } from 'src/dto/user-dto';
 import { BackendEvents } from 'src/events/backend-events.enum';
 import { EmailParsedEvent } from 'src/events/email-parsed.event';
 import { ParsedEmail } from 'src/types/parsed-email';
@@ -30,9 +32,6 @@ export class MessagesService {
   async createMessage(message: CreateMessageDTO): Promise<string> {
     console.log('MessagesService:createMessage');
     console.log(JSON.stringify(message,null,2));
-    console.log('Checking Env Variables');
-    console.log(DOMAIN_NAME);
-    console.log(SENDGRID_API_KEY);
 
     const observable = this.http.post(`${DB_ACCESSOR_URL}/${this.path}`, message).pipe(
       tap(response => console.log(response)),
@@ -51,22 +50,28 @@ export class MessagesService {
     return firstValueFrom(observable);
   }
 
-  private toEmail(message: MessageDTO, to: string): EmailDTO {
+  private async sendEmailToChatGroup(message: MessageDTO): Promise<void> {
+    const email = await this.createEmailForChatGroup(message);
+    this.emailService.sendEmail(email);
+  }
+
+  private async createEmailForChatGroup(message: MessageDTO): Promise<EmailDTO> {
+    const users = await this.userService.getUsers();
+    const author = users.find(u => u._id === message.userId);
+    const to = this.getEmailsOfChatGroup(users, message.userId);
     return {
       to,
-      from: { name: 'Rick Sanches', email: `our.chat@${DOMAIN_NAME}` },
+      from: { name: author.nickName, email: `${CHAT_NAME}@${DOMAIN_NAME}` },
       text: message.content,
-      subject: 'A message was sent via chat'
+      subject: 'Sending message via chat'
     }
   }
 
-  private async sendEmailToChatGroup(message: MessageDTO): Promise<any> {
-    const to = 'holtzilya2008@gmail.com';
-    const email = this.toEmail(message, to);
-    console.log(`Emiting sending eamil`);
-    return Promise.resolve();
-    // this.emailService.sendEmail(email);
+  private getEmailsOfChatGroup(users: UserDTO[], messageAuthorId: string): EmailData[] {
+    return users.filter(u => u._id !== messageAuthorId)
+                .map(u => ({ name: u.nickName, email: u.email }));
   }
+
 
   @OnEvent(BackendEvents.EmailParsed)
   private handleEmailParsed(payload: EmailParsedEvent) {
@@ -74,9 +79,7 @@ export class MessagesService {
   }
 
   private async createMessageFromEmail(email: ParsedEmail): Promise<void> {
-    console.log('createMessageFromEmail');
     const user = await this.userService.getUserByEmail(email.from);
-    console.log(`user: \n ${JSON.stringify(user)}`);
     const message: CreateMessageDTO = {
       userId: user._id,
       content: email.text
