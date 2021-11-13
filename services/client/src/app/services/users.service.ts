67100585d2a5cfe43e16cd354a4b0df213467897
Ilya <ilyah@codevalue.net>
2021-11-13 04:26:52 +0200

    updated users error on client

--- services/client/src/app/services/users.service.ts
+++ services/client/src/app/services/users.service.ts
@@ -10,13 +10,15 @@ import { UsersApiService } from './users-api.service';
 export class UsersService {
 
   private users: UserDTO[];
-  private currentUser = new BehaviorSubject<UserDTO>(UsersMock[0]);
+  private currentUser = new BehaviorSubject<UserDTO>(null);
   currentUser$ = this.currentUser.asObservable();
 
   constructor(private api: UsersApiService) { }
 
   async initUsers(): Promise<void> {
     this.users = await this.api.getUsers().toPromise();
+    const loginAs = this.users.find(u => u.email === 'holtzilya2008@gmail.com');
+    this.currentUser.next(loginAs);
   }
 
   getUsers(): UserDTO[] {
@@ -24,7 +26,7 @@ export class UsersService {
   }
 
   getUserById(userId: string): UserDTO {
-      return this.users.find(u => u._id === userId) ?? this.users[0];
+      return this.users.find(u => u._id === userId);
   }
 
   getCurrentUser(): UserDTO {
