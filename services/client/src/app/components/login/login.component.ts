8303c9d1ecc4b8b5dbf56063e25a367b8ed61351
Ilya <ilyah@codevalue.net>
2021-11-15 18:08:39 +0200

    merged chiplist bugfix again

--- services/client/src/app/components/login/login.component.ts
+++ services/client/src/app/components/login/login.component.ts
@@ -12,19 +12,12 @@ export class LoginComponent implements OnInit {
 
   email: string;
   availableEmails: string[];
-  isInitialized = false;
 
   constructor(private userService: UsersService,
               private router: Router) { }
 
   ngOnInit(): void {
-    this.userService.isInitialized$.pipe(
-      filter(value => !!value),
-      take(1)
-    ).subscribe((isInitialized) => {
-      this.isInitialized = isInitialized;
-      this.availableEmails = this.userService.getUsers().map(u => u.email);
-    });
+    this.availableEmails = this.userService.getUsers().map(u => u.email);
   }
 
   login() {
